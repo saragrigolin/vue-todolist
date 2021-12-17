f369ac71792d6371707b9bc97b488b4b92c14492
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-17 12:53:15 +0100

    add Vue

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 */
+
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            todos: [
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                }, 
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                }
+            ]
+        }
+    }
+)
