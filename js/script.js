c01169b992f9dce29bffbb05bf1f690cc8cec023
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-17 14:29:11 +0100

    bonus 1 and 2

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ MILESTONE 2
 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
 
 const app = new Vue (
